73a243dbac9f3365c38c9a16e235ea62217c86a3
Baskaran <baskarma63@gmail.com>
2025-10-29 12:59:30 +0530

    Update PerfectNumbers.java

--- Problem Solving /Day2/PerfectNumbers.java
+++ Problem Solving /Day2/PerfectNumbers.java
@@ -1,16 +1,24 @@
- static void PerfectNumber(int a) {
-        int count = 0;
-        for (int i = 1; i < a; i++) {
-            if (a % i == 0) {
+public class main {
+
+    static boolean isPerfect(int n) {
+        if (n == 1) {
+            return false;
+        }
+        int count = 1;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 count += i;
+                if (i != n / i) {
+                    count += n / i;
+                }
             }
         }
-        if (a == count) {
-            System.out.println(a + " is a perfect number..");
-        } else {
-            System.out.println(a + " is not a Perfect Number..");
-        }
+        return n==count;
     }
 
-
-//This is a static function ..
+    public static void main(String[] args) {
+        System.out.println(isPerfect(6));
+        System.out.println(isPerfect(28));
+        System.out.println(isPerfect(19));
+    }
+}
