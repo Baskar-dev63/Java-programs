ead62d56d603df5b9c9820935f08b8db629dbf83
Baskaran <baskarma63@gmail.com>
2025-11-03 18:23:09 +0530

    Update primenumber.java

--- Problem Solving /Day2/find primenumber.java
+++ Problem Solving /Day2/find primenumber.java
@@ -24,3 +24,42 @@ public class main {
 
     }
 }
+
+
+
+
+class prime {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Starting Number: ");
+        int a = scanner.nextInt();
+        System.out.println("Enter the end of range: ");
+        int b = scanner.nextInt();
+
+        if (a == 1) {
+            System.out.println(a + " is Not a Prime Number.");
+            a = 2;
+        }
+
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            boolean isPrime = true;
+            
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime) {
+                count += 1;
+                System.out.println(i + " is " + count + " Prime Number..");
+            }
+            
+        }
+        
+    }
+}
+
