34c31bfcf899138b94e8dc106dbd997d03d44d45
Baskaran <baskarma63@gmail.com>
2025-12-24 19:20:49 +0530

    Update practices4.java

--- Academy tasks/practices4.java
+++ Academy tasks/practices4.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class main {
     public static String numberFinder(int a) {
         if (a > 0) {
-            return "Positive Number";
-        } else if (a < 0) {
+            if (a % 2 == 0)
+                return "Positive Even Number";
+            else
+                return "Positive Add Number";
+        } else if (a < 0)
             return "Negaitve Number";
-        } else {
+        else
             return "Number is Zero";
-        }
+
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try{
-            System.out.println("Enter a Number:");
-            int a = scanner.nextInt();
-            System.out.println(numberFinder(a));
-        }catch(Exception err){
-            System.out.println("Numbers Only Acceptable.");
+        while (true) {
+            try {
+                System.out.println("Enter a Number:");
+                int a = scanner.nextInt();
+                System.out.println(numberFinder(a));
+                break;
+            } catch (Exception err) {
+                System.out.println("Your Not given a Proper Value");
+                scanner.nextLine();
+            }
         }
+        scanner.close();
     }
 }
